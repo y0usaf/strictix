887b84123e6d7942f7bab1a8c04b9e9ff422c704
y0usaf <74448287+y0usaf@users.noreply.github.com>
2026-03-11 18:14:35 -0400

    Simplify lint metadata matching

--- bin/src/utils.rs
+++ bin/src/utils.rs
@@ -3,24 +3,18 @@ use std::collections::HashMap;
 use lib::{LINTS, Lint};
 use rnix::SyntaxKind;
 
-#[allow(clippy::borrowed_box)]
-pub fn lint_map_of(
-    lints: &[&'static Box<dyn Lint>],
-) -> HashMap<SyntaxKind, Vec<&'static Box<dyn Lint>>> {
+pub fn lint_map_of(lints: &[&'static dyn Lint]) -> HashMap<SyntaxKind, Vec<&'static dyn Lint>> {
     let mut map = HashMap::new();
     for lint in lints {
-        let lint = *lint;
-        let matches = lint.match_kind();
-        for m in matches {
+        for &m in lint.match_kind() {
             map.entry(m)
-                .and_modify(|v: &mut Vec<_>| v.push(lint))
-                .or_insert_with(|| vec![lint]);
+                .and_modify(|v: &mut Vec<_>| v.push(*lint))
+                .or_insert_with(|| vec![*lint]);
         }
     }
     map
 }
 
-#[allow(clippy::borrowed_box)]
-pub fn lint_map() -> HashMap<SyntaxKind, Vec<&'static Box<dyn Lint>>> {
+pub fn lint_map() -> HashMap<SyntaxKind, Vec<&'static dyn Lint>> {
     lint_map_of(&LINTS)
 }
